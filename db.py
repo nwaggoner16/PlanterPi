e3c7dfb1cf5d12f7089e6c0a344d05cf666cdb0f
Nathan <nwaggoner16@gmail.com>
2020-06-18 18:38:16 -0400

    Random macbook changes

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ import numpy
 
 #Initializing config object
 config = configparser.ConfigParser()
-config.read("/home/pi/PlanterPi/.config.ini")
+config.read("/Users/nathanwaggoner/PlanterPi/config.ini")
 
 #Getting db connection info
 host = config.get('credentials','host')
@@ -20,7 +20,7 @@ db = config.get('credentials','db')
 #Initializing db connection
 mysql = mariadb.connect(user=user, password=passwd, database=db)
 cursor = mysql.cursor()
-		
+
 class planter():
 	def __init__(self, planter_name):
 		#Setting planter variables
@@ -28,38 +28,38 @@ class planter():
 		self.planter_name = planter_name
 		self.planter_id = self.planter_header[0]
 		self.cons_days_watered = self.planter_header[2]
-		self.times_watered = self.planter_header[3]
+		#self.times_watered = self.planter_header[3]
 		self.planter_sensor_data = self.get_planter_sensor_data(self.planter_id)
 		self.run_speed = self.planter_header[4]
 		self.run_time = self.planter_header[5]
 		self.target_moisture = self.planter_header[6]
-	
-	def get_planter_header_data(self, planter_name):	
+
+	def get_planter_header_data(self, planter_name):
 		#Getting planter header info from planter table using planter_name
-		temp_query = "SELECT planter_id, planter_name, cons_days_watered, times_watered, run_speed, run_time, target_moisture FROM Plant_logger.planter WHERE planter_name = '{}';".format(planter_name)
+		temp_query = "SELECT planter_id, planter_name, cons_days_watered, run_speed, run_time, target_moisture FROM PlanterPi.planter WHERE planter_name = '{}';".format(planter_name)
 		cursor.execute(temp_query)
-		
+
 		#Declaring numpy array to store header info
 		planter_header = []
 		for arr in cursor:
 			planter_header = arr
-			
+
 		planter_header = numpy.array(planter_header)
-			
+
 		return planter_header;
 
 	def get_planter_sensor_data(self, planter_id):
 		#Getting planter sensor data
-		sensor_data_query = "SELECT planter_id, log_id, log_time, temperature, humidity, light, soil_temperature, soil_moisture FROM Plant_logger.sensor_data WHERE planter_id = {};".format(planter_id)
+		sensor_data_query = "SELECT planter_id, log_id, log_time, temperature, humidity, light, soil_temperature, soil_moisture FROM PlanterPi.sensor_data WHERE planter_id = {};".format(planter_id)
 		cursor.execute(sensor_data_query)
-		
+
 		#Storing sensor data into numpy array
 		planter_sesnor_data = []
 		for row in cursor:
-			planter_sesnor_data.append(row);	
-				
+			planter_sesnor_data.append(row);
+
 		planter_array_np = numpy.array(planter_sesnor_data)
-		
+
 		return planter_array_np
 
 	def avg_moisture(self, hours=24):
@@ -71,7 +71,7 @@ class planter():
 			moisture_sum += p[7];
 		moisture_avg =  moisture_sum/hours
 		return moisture_avg
-		
+
 	def avg_temperature(self, hours=24):
 		temperature_sum = 0
 		temperature_avg = 0
@@ -80,8 +80,8 @@ class planter():
 		for p in last_hours_temp_np:
 			temperature_sum += p[7];
 		temperature_avg =  temperature_sum/hours
-		return temperature_avg	
-	
+		return temperature_avg
+
 	def avg_humidity(self, hours=24):
 		humidity_sum = 0
 		humidity_avg = 0
@@ -95,12 +95,12 @@ class planter():
 	def update_cons_days_watered(self, days):
 		cursor.execute('update planter set cons_days_watered = {} where planter_id = {}'.format(days, self.planter_id))
 		mysql.commit()
-		
+
 	def water_logging(self, run_time, run_speed):
-		cursor.execute('insert into Plant_logger.water_log (planter_id, run_time_sec, run_speed) values ({}, {}, {})'.format(self.planter_id, run_time, run_speed))
+		cursor.execute('insert into PlanterPi.water_log (planter_id, run_time_sec, run_speed) values ({}, {}, {})'.format(self.planter_id, run_time, run_speed))
 		mysql.commit()
-		
-		
+
+
 class planter_by_id():
 	def __init__(self, planter_id):
 		#Setting planter variables
@@ -108,38 +108,38 @@ class planter_by_id():
 		self.planter_name = self.planter_header[1]
 		self.planter_id = planter_id
 		self.cons_days_watered = self.planter_header[2]
-		self.times_watered = self.planter_header[3]
+		#self.times_watered = self.planter_header[3]
 		self.planter_sensor_data = self.get_planter_sensor_data()
-		self.run_speed = self.planter_header[4]
-		self.run_time = self.planter_header[5]
-		self.target_moisture = self.planter_header[6]
-	
-	def get_planter_header_data(self, planter_id):	
+		self.run_speed = self.planter_header[3]
+		self.run_time = self.planter_header[4]
+		self.target_moisture = self.planter_header[5]
+
+	def get_planter_header_data(self, planter_id):
 		#Getting planter header info from planter table using planter_name
-		temp_query = "SELECT planter_id, planter_name, cons_days_watered, times_watered, run_speed, run_time, target_moisture FROM Plant_logger.planter WHERE planter_id = {};".format(planter_id)
+		temp_query = "SELECT planter_id, planter_name, cons_days_watered, run_speed, run_time, target_moisture FROM PlanterPi.planter WHERE planter_id = {};".format(planter_id)
 		cursor.execute(temp_query)
-		
+
 		#Declaring numpy array to store header info
 		planter_header = []
 		for arr in cursor:
 			planter_header = arr
-			
+
 		planter_header = numpy.array(planter_header)
-			
+
 		return planter_header;
 
 	def get_planter_sensor_data(self):
 		#Getting planter sensor data
-		sensor_data_query = "SELECT planter_id, log_id, log_time, temperature, humidity, light, soil_temperature, soil_moisture FROM Plant_logger.sensor_data WHERE planter_id = {};".format(self.planter_id)
+		sensor_data_query = "SELECT planter_id, log_id, log_time, temperature, humidity, light, soil_temperature, soil_moisture FROM PlanterPi.sensor_data WHERE planter_id = {};".format(self.planter_id)
 		cursor.execute(sensor_data_query)
-		
+
 		#Storing sensor data into numpy array
 		planter_sesnor_data = []
 		for row in cursor:
-			planter_sesnor_data.append(row);	
-				
+			planter_sesnor_data.append(row);
+
 		planter_array_np = numpy.array(planter_sesnor_data)
-		
+
 		return planter_array_np
 
 	def avg_moisture(self, hours=24):
@@ -151,7 +151,7 @@ class planter_by_id():
 			moisture_sum += p[7];
 		moisture_avg =  moisture_sum/hours
 		return moisture_avg
-		
+
 	def avg_temperature(self, hours=24):
 		temperature_sum = 0
 		temperature_avg = 0
@@ -160,8 +160,8 @@ class planter_by_id():
 		for p in last_hours_temp_np:
 			temperature_sum += p[3];
 		temperature_avg =  temperature_sum/hours
-		return temperature_avg	
-	
+		return temperature_avg
+
 	def avg_humidity(self, hours=24):
 		humidity_sum = 0
 		humidity_avg = 0
@@ -171,7 +171,7 @@ class planter_by_id():
 			humidity_sum += p[4];
 		humidity_avg =  humidity_sum/hours
 		return humidity_avg
-		
+
 	def avg_light(self, hours=24):
 		light_sum = 0
 		light_avg = 0
@@ -181,7 +181,7 @@ class planter_by_id():
 			light_sum += p[5];
 		light_avg =  light_sum/hours
 		return light_avg
-		
+
 	def avg_soil_temperature(self, hours=24):
 		soil_temperature_sum = 0
 		soil_temperature_avg = 0
@@ -195,31 +195,31 @@ class planter_by_id():
 	def update_cons_days_watered(self, days):
 		cursor.execute('update planter set cons_days_watered = {} where planter_id = {}'.format(days, self.planter_id))
 		mysql.commit()
-		
+
 	def water_logging(self, run_time, run_speed):
-		cursor.execute('insert into Plant_logger.water_log (planter_id, run_time_sec, run_speed) values ({}, {}, {})'.format(self.planter_id, run_time, run_speed))
+		cursor.execute('insert into PlanterPi.water_log (planter_id, run_time_sec, run_speed) values ({}, {}, {})'.format(self.planter_id, run_time, run_speed))
 		mysql.commit()
-		
+
 	def update_run_time(self, rt):
 		cursor.execute('update planter set run_time = {} where planter_id = {}'.format(rt, self.planter_id))
 		self.run_time = rt
-		
+
 	def update_run_speed(self, rs):
 		cursor.execute('update planter set run_speed = {} where planter_id = {}'.format(rs, self.planter_id))
-		
+
 	def plot_moisture_data(self, hours):
 		plot_array_time = []
 		cursor.execute('select log_time from sensor_data where planter_id = {} order by log_time desc limit={};'.format(self.planter_id, hours))
 		for n in cursor:
 			plot_array_time.append(n[0])
-			
+
 		plot_array_moisture = []
 		cursor.execute('select soil_moisture from sensor_data where planter_id = {} order by log_time desc limit={};'.format(self.planter_id, hours))
 		for n in cursor:
 			plot_array_moisture.append(n[0])
-			
+
 		return (plot_array_time), (plot_array_moisture)
-		
+
 	def plot_moisture_data_test(self, hours):
 		plot_array_time = []
 		plot_array_moisture=[]
@@ -228,21 +228,21 @@ class planter_by_id():
 		for pl in plot_var_time:
 			plot_array_time.append(pl[2])
 			plot_array_moisture.append(pl[7])
-		return (plot_array_time), (plot_array_moisture) 
-		
+		return (plot_array_time), (plot_array_moisture)
+
 	def get_log_times(self):
 		return self.planter_sensor_data[:, 2]
-		
+
 	def get_soil_moisture(self):
 		return self.planter_sensor_data[:, 7]
-		
+
 	def get_temperature(self):
 		return self.planter_sensor_data[:, 3]
-	
+
 	def get_soil_temperature(self):
 		return self.planter_sensor_data[:, 6]
-		
-	
+
+
 
 class general_data():
 	def get_planter_names(self):
@@ -251,13 +251,12 @@ class general_data():
 		for n in cursor:
 			planter_name_arr.append(n)
 		return planter_name_arr
-	
-	
-	
-		
-# DEBUG		
-#rongo = planter_by_id(2)		
-#print(rongo.get_soil_moisture())
+
+
+
+
+# DEBUG
+rongo = planter_by_id(2)
+print(rongo.get_soil_moisture())
 #debug_ob = general_data()
 #print(debug_ob.get_planter_names())
-
